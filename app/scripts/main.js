afcf43e8a6d47dd30e3cf12315e6ea266a0a5627
Nicolas Emery <Nicolas5150@gmail.com>
2017-04-09 15:18:52 -0400

    Removing Content
    Reordering code to fit style guide as drawn. Only needing 2 pages with
    vertical scroll and the second having a horizontal scroll to present
    all the data / history of each jeep era.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,19 +11,19 @@ $(document).ready(function() {
 
   // fullpage customization
   $('#fullpage').fullpage({
-    sectionsColor: ['#374140', '#B8AE9C', '#B8AE9C', '#B8AE9C', '#B8AE9C'],
+    sectionsColor: ['#374140', '#B8AE9C'],
     sectionSelector: '.vertical-scrolling',
     slideSelector: '.horizontal-scrolling',
     navigation: true,
     slidesNavigation: true,
     controlArrows: false,
-    anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+    anchors: ['firstSection', 'secondSection'],
     menu: '#menu',
 
     afterLoad: function(anchorLink, index) {
       $header_top.css('background', 'rgba(0, 47, 77, .3)');
       $nav.css('background', 'rgba(0, 47, 77, .25)');
-      if (index == 5) {
+      if (index == 2) {
           $('#fp-nav').hide();
       }
 
@@ -37,31 +37,31 @@ $(document).ready(function() {
     },
 
     onLeave: function(index, nextIndex, direction) {
-      if(index == 5) {
+      if(index == 2) {
         $('#fp-nav').show();
       }
     },
 
     afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex) {
-      if(anchorLink == 'fifthSection' && slideIndex == 1) {
-        $.fn.fullpage.setAllowScrolling(false, 'up');
-        $header_top.css('background', 'transparent');
-        $nav.css('background', 'transparent');
-        $(this).css('background', '#374140');
-        $(this).find('h2').css('color', 'white');
-        $(this).find('h3').css('color', 'white');
-        $(this).find('p').css(
-          {
-            'color': '#DC3522',
-            'opacity': 1,
-            'transform': 'translateY(0)'
-          }
-        );
-      }
+      // if(anchorLink == 'secondSection' && slideIndex == 1) {
+      //   $.fn.fullpage.setAllowScrolling(false, 'up');
+      //   $header_top.css('background', 'transparent');
+      //   $nav.css('background', 'transparent');
+      //   $(this).css('background', '#374140');
+      //   $(this).find('h2').css('color', 'white');
+      //   $(this).find('h3').css('color', 'white');
+      //   $(this).find('p').css(
+      //     {
+      //       'color': '#DC3522',
+      //       'opacity': 1,
+      //       'transform': 'translateY(0)'
+      //     }
+      //   );
+      // }
     },
 
     onSlideLeave: function( anchorLink, index, slideIndex, direction) {
-      if(anchorLink == 'fifthSection' && slideIndex == 1) {
+      if(anchorLink == 'secondSection' && slideIndex == 1) {
         $.fn.fullpage.setAllowScrolling(true, 'up');
         $header_top.css('background', 'rgba(0, 47, 77, .3)');
         $nav.css('background', 'rgba(0, 47, 77, .25)');
