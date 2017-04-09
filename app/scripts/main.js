1296c1910546d68bc6fbf4acd9f104287d238774
Nicolas Emery <Nicolas5150@gmail.com>
2017-04-09 14:19:25 -0400

    Testing New Approach
    Adding in a full page js codpen example by George Martsoukos and using
    some of his logic to create the basis of the Jeep history website.
    Adding the jeep animation works fine.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,78 @@
-$(document).ready(function() {
-   $('#fullpage').fullpage();
+// $(document).ready(function() {
+//    $('#fullpage').fullpage();
+// });
+//
+//
+//
+// $('.box-wrapper').each(function(index, element) {
+//
+//     setTimeout(function(){
+//         element.classList.remove('loading');
+//     }, index * 500);
+//
+// });
+
+// variables
+var $header_top = $('.header-top');
+var $nav = $('nav');
+
+
+
+// toggle menu
+$header_top.find('a').on('click', function() {
+  $(this).parent().toggleClass('open-menu');
 });
 
 
 
-$('.box-wrapper').each(function(index, element) {
+// fullpage customization
+$('#fullpage').fullpage({
+  sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
+  sectionSelector: '.vertical-scrolling',
+  slideSelector: '.horizontal-scrolling',
+  navigation: true,
+  slidesNavigation: true,
+  controlArrows: false,
+  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+  menu: '#menu',
 
-    setTimeout(function(){
-        element.classList.remove('loading');
-    }, index * 500);
+  afterLoad: function(anchorLink, index) {
+    $header_top.css('background', 'rgba(0, 47, 77, .3)');
+    $nav.css('background', 'rgba(0, 47, 77, .25)');
+    if (index == 5) {
+        $('#fp-nav').hide();
+      }
+  },
 
-});
+  onLeave: function(index, nextIndex, direction) {
+    if(index == 5) {
+      $('#fp-nav').show();
+    }
+  },
+
+  afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex) {
+    if(anchorLink == 'fifthSection' && slideIndex == 1) {
+      $.fn.fullpage.setAllowScrolling(false, 'up');
+      $header_top.css('background', 'transparent');
+      $nav.css('background', 'transparent');
+      $(this).css('background', '#374140');
+      $(this).find('h2').css('color', 'white');
+      $(this).find('h3').css('color', 'white');
+      $(this).find('p').css(
+        {
+          'color': '#DC3522',
+          'opacity': 1,
+          'transform': 'translateY(0)'
+        }
+      );
+    }
+  },
+
+  onSlideLeave: function( anchorLink, index, slideIndex, direction) {
+    if(anchorLink == 'fifthSection' && slideIndex == 1) {
+      $.fn.fullpage.setAllowScrolling(true, 'up');
+      $header_top.css('background', 'rgba(0, 47, 77, .3)');
+      $nav.css('background', 'rgba(0, 47, 77, .25)');
+    }
+  }
+}); 
